a238f29b4072ee784a73ab99e686a6774fe6b903
S1MBIK <zuravko@bk.ru>
2025-04-23 08:52:30 +0300

    day9

---
+++ day9/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+enum Post
+{
+    JuniorDeveloper = 160,
+    Developer = 180,
+    SeniorDeveloper = 200,
+    Manager = 220,
+    Director = 240
+}
+
+class Accountant
+{
+    public bool AskForBonus(Post worker, int hours)
+    {
+        return hours > (int)worker;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Accountant accountant = new Accountant();
+
+        
+        Post worker = Post.Developer;
+        Console.WriteLine("введите времмя сотрудника");
+        int hoursWorked = Convert.ToInt32(Console.ReadLine());
+
+        bool bonusEligible = accountant.AskForBonus(worker, hoursWorked);
+
+        if (bonusEligible)
+        {
+            Console.WriteLine("Сотруднику положена премия.");
+        }
+        else
+        {
+            Console.WriteLine("Сотруднику премия не положена.");
+        }
+    }
+}
