b407dc5a599dfdf393e8d2f8659b072453facbd9
S1MBIK <zuravko@bk.ru>
2025-04-23 08:59:46 +0300

    Update Program.cs

--- day9/ConsoleApp2/Program.cs
+++ day9/ConsoleApp2/Program.cs
@@ -23,9 +23,24 @@ class Program
     {
         Accountant accountant = new Accountant();
 
-        
-        Post worker = Post.Developer;
-        Console.WriteLine("введите времмя сотрудника");
+        Console.WriteLine("Выберите должность сотрудника:");
+        foreach (var post in Enum.GetValues(typeof(Post)))
+        {
+            Console.WriteLine($"{(int)post}. {post}");
+        }
+
+        Console.Write("Введите номер должности: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        if (!Enum.IsDefined(typeof(Post), choice))
+        {
+            Console.WriteLine("Некорректный выбор. Программа завершена.");
+            return;
+        }
+
+        Post worker = (Post)choice; 
+
+        Console.WriteLine("Введите количество отработанных часов: ");
         int hoursWorked = Convert.ToInt32(Console.ReadLine());
 
         bool bonusEligible = accountant.AskForBonus(worker, hoursWorked);
