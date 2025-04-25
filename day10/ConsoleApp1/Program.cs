b2bfd4ad5f7ad0561e1612d9fd78ad8df33ec7dd
S1MBIK <zuravko@bk.ru>
2025-04-25 09:10:03 +0300

    day10 and day11

---
+++ day10/ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Product
+{
+    private int quantity;
+    private double price;
+
+    public Product(int quantity, double price)
+    {
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int GetQuantity()
+    {
+        return quantity;
+    }
+
+    public double GetPrice()
+    {
+        return price;
+    }
+
+    public virtual double Cost()
+    {
+        return quantity * price;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"количество: {quantity}, цена: {price}");
+    }
+}
+
+class Markers : Product
+{
+    private string name;
+    private int grade;
+
+    public Markers(int quantity, double price, string name, int grade)
+        : base(quantity, price)
+    {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public override double Cost()
+    {
+        return base.Cost() / Math.Sqrt(grade);
+    }
+
+    public void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"название: {name}, сорт: {grade}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product product = new Product(10, 50);
+        Markers markers = new Markers(20, 30, "фломастеры для рисования ", 2);
+
+        Console.WriteLine("ифнормация о продукте:");
+        product.DisplayInfo();
+        Console.WriteLine($"стоимость товара: {product.Cost()}");
+
+        Console.WriteLine("\nинформация о фломастерах:");
+        markers.DisplayInfo();
+        Console.WriteLine($"Стоимость фломастера: {markers.Cost():F2}");
+    }
+}
---
+++ day10/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class A
+{
+    private int a = 5;
+    private int b = 10;
+
+    public int c
+    {
+        get { return a + b; }
+    }
+
+    public A() { }
+}
+
+class B : A
+{
+    private int d = 15;
+
+    public int c2
+    {
+        get
+        {
+            int result = c + d; 
+            return result > 20 ? result : 0; 
+        }
+    }
+
+ 
+    public B() : base() { }
+
+    public B(int dValue) : base()
+    {
+        d = dValue;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A a = new A();
+        Console.WriteLine($"Значение свойства c класса A: {a.c}");
+       
+        B b1 = new B();
+        Console.WriteLine($"Значение свойства c2 класса B (по умолчанию): {b1.c2}");
+
+        B b2 = new B(25);
+        Console.WriteLine($"Значение свойства c2 класса B (собственный конструктор): {b2.c2}");
+    }
+}
