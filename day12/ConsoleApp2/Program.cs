f030c1a29a7d7f8bab4557ee9b8a5cd302f07e55
S1MBIK <zuravko@bk.ru>
2025-04-26 12:27:55 +0300

    day12

---
+++ day12/ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<double, double, double> Add = (a, b) => a + b;
+        Func<double, double, double> Sub = (a, b) => a - b;
+        Func<double, double, double> Mul = (a, b) => a * b;
+        Func<double, double, double> Div = (a, b) =>
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Ошибка: деление на ноль.");
+            }
+            return a / b;
+        };
+
+        Console.WriteLine("Выберите операцию: +, -, *, /");
+        string operation = Console.ReadLine();
+
+        Console.Write("Введите первое число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result;
+
+        switch (operation)
+        {
+            case "+":
+                result = Add(num1, num2);
+                break;
+            case "-":
+                result = Sub(num1, num2);
+                break;
+            case "*":
+                result = Mul(num1, num2);
+                break;
+            case "/":
+                try
+                {
+                    result = Div(num1, num2);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("Некорректная операция.");
+                return;
+        }
+
+        Console.WriteLine($"Результат: {result}");
+    }
+}
