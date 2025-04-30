a8fef5519e3d10b6ba2832e54d594c0cf2aefe80
S1MBIK <zuravko@bk.ru>
2025-04-30 09:03:42 +0300

    day13

---
+++ day13/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            MyInfo myInfo = new MyInfo();
+
+            // Подписываемся на событие
+            myInfo.NameChanged += (sender, newName) =>
+            {
+                Console.WriteLine($"Имя было изменено на: {newName}");
+            };
+
+            // Устанавливаем имя
+            myInfo.Name = "Иван";
+
+            // Пробуем установить пустое имя (вызовет исключение)
+            myInfo.Name = "";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Обработка исключения: {ex.Message}");
+        }
+    }
+}
